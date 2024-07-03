0e988591812b02637d610c5752e29660596724ac
Roshane Moraes <moraes.git@gmail.com>
2024-07-03 20:38:33 +0530

    Complete feature/Add-employee with validation

--- src/main/java/com/sep/backend_noAuth/controller/Postmaster/EmployeeController.java
+++ src/main/java/com/sep/backend_noAuth/controller/Postmaster/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.sep.backend_noAuth.controller.Postmaster;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sep.backend_noAuth.dto.AddEmployeeDto;
+import com.sep.backend_noAuth.entity.Employee;
+import com.sep.backend_noAuth.repository.EmployeeRepository;
+import com.sep.backend_noAuth.service.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +15,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/postmaster/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
     @GetMapping("live-map")
     public List<Map<String, Object>> getEmployees() {
         List<Map<String, Object>> employees = Arrays.asList(
@@ -44,5 +54,25 @@ public class EmployeeController {
                 )
         );
         return employees;
+    }
+
+    @GetMapping("list-employee")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    @PostMapping("add-employee")
+    public ResponseEntity<String> addNewEmployee(@RequestBody AddEmployeeDto employee){
+        Employee newEmployee = new Employee();
+        newEmployee.setId(Long.valueOf(sequenceGeneratorService.getSequenceNumber(Employee.SEQUENCE_NAME)));
+        newEmployee.setUserName(employee.getEmployeeUserName());
+        newEmployee.setFullName(employee.getEmployeeFullName());
+        newEmployee.setEmail(employee.getEmployeeEmail());
+        newEmployee.setNic(employee.getEmployeeNIC());
+        newEmployee.setContactNumber(employee.getEmployeeContactNumber());
+        newEmployee.setDateJoined(employee.getEmployeeDateJoined());
+        newEmployee.setPassword(employee.getAccountPassword());
+        newEmployee.setRoles(employee.getRole());
+        employeeRepository.save(newEmployee);
+        return ResponseEntity.ok("New Employee Added.");
     }
 }
--- src/main/java/com/sep/backend_noAuth/entity/Address.java
+++ src/main/java/com/sep/backend_noAuth/entity/Address.java
@@ -1,5 +1,7 @@
 package com.sep.backend_noAuth.entity;
 import java.util.List;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -12,14 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 
-@Document(collection = "address" )
+@Document(collection = "Address" )
 public class Address {
+    @Id
     String addressId;
     String textForm;
     String lat;
+    String lng;
     String city;
     String zone;
     String houseNumber;
-    String lng;
     List<String> members;
 }

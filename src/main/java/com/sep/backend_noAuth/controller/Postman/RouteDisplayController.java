2a2d3255093edfb343f3af7118bd79d832a37292
Nandaka TTD <129133244+Tharindu-Deshan@users.noreply.github.com>
2024-08-26 21:25:08 +0530

    routedisplaycontroller

--- src/main/java/com/sep/backend_noAuth/controller/Postman/RouteDisplayController.java
+++ src/main/java/com/sep/backend_noAuth/controller/Postman/RouteDisplayController.java
@@ -4,25 +4,28 @@ import com.sep.backend_noAuth.entity.Delivery;
 import com.sep.backend_noAuth.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @RestController
-@RequestMapping("/api/postman/view-delivery")
-public class ViewDeliveryController {
+@RequestMapping("api/postman/route-display")
+public class RouteDisplayController {
 
     @Autowired
     private DeliveryRepository deliveryRepository;
 
-    @GetMapping("/get-today-delivery/{postmanId}")
-    public Delivery getTodayDelivery(@PathVariable String postmanId){
+    @GetMapping("/get-delivery")
+    public Delivery getDelivery(@RequestParam String postmanId){
+
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
         String date = currentDate.format(formatter);
-        return deliveryRepository.findByDateAndPostmanId(date, postmanId);
+
+        return deliveryRepository.findByDateAndPostmanId(date,postmanId);
     }
+
 }

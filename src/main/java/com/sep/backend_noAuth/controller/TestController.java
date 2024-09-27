93443fae4a59fab15a413913ae6c7d994afe2b82
Roshane Moraes <moraes.git@gmail.com>
2024-09-27 22:54:16 +0530

    Update CI/CD

--- src/main/java/com/sep/backend_noAuth/controller/TestController.java
+++ src/main/java/com/sep/backend_noAuth/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello(){
-        return "Hello from the other side.";
+        return "Hello from the other side.......";
     }
 }

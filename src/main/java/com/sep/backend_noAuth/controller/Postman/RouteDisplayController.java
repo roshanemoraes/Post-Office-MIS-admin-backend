dbde2515f73508b9e49e14dcc6a7fbf8a761e1f0
Nandaka TTD <129133244+Tharindu-Deshan@users.noreply.github.com>
2024-10-09 13:57:47 +0530

    route-display-committed

--- src/main/java/com/sep/backend_noAuth/controller/Postman/RouteDisplayController.java
+++ src/main/java/com/sep/backend_noAuth/controller/Postman/RouteDisplayController.java
@@ -3,6 +3,9 @@ package com.sep.backend_noAuth.controller.Postman;
 import com.sep.backend_noAuth.entity.Delivery;
 import com.sep.backend_noAuth.repository.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,18 +17,23 @@ import java.time.format.DateTimeFormatter;
 @RestController
 @RequestMapping("api/postman/route-display")
 public class RouteDisplayController {
-
     @Autowired
     private DeliveryRepository deliveryRepository;
 
     @GetMapping("/get-delivery")
-    public Delivery getDelivery(@RequestParam String postmanId){
+    public ResponseEntity<Delivery> getDelivery(@RequestParam String postmanId){
 
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
         String date = currentDate.format(formatter);
 
-        return deliveryRepository.findByDateAndPostmanId(date,postmanId);
+        Delivery delivery =deliveryRepository.findByDateAndPostmanId(date,postmanId);
+        if (delivery == null){
+            System.out.println();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(delivery);
+
     }
 
 }
